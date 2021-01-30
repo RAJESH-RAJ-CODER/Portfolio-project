697dd7d84f11a112f9b4f8a55535c6ee76934cbc
Rajesh Raj <rajeshmca124@gmal.com>
2021-01-30 22:53:52 +0530

    NavBar is Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <>
-    <h2>Hello Rajesh</h2>
+    <Navbar />
     
 </>
   );
