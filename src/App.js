fa89caa9493693774bfc7f3c7b1d19a48777d14d
Rajesh Raj <rajeshmca124@gmal.com>
2021-01-31 12:00:11 +0530

    Header is completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import Header from './Components/Header';
 import Navbar from './Components/Navbar';
-
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+    <Particles 
+    params={{
+      particles:{
+        number:{
+          value:30,
+        density:{
+          enable:true,
+          value_area:900
+        }
+        },
+        shape:{
+          type:"circle",
+          stroke:{
+            width:6,
+            color:'#3CA9D1'
+          }
+        }
+      }
+    }}
+    />
     <Navbar />
     <Header />
     
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typed from 'react-typed';
 
+
 const Header = () => {
     return (
         <div className="header-wrapper">
